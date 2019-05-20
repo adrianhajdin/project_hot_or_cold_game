76e91089413c4166736fcd34d034734494575448
adrian <adrianhajdin00@gmail.com>
2019-05-20 17:21:42 +0200

    changes

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -16,7 +16,7 @@ const Form = ({ block, returnGuessToApp }) => {
 
   return (
     <form style={{marginTop: '20px'}} onSubmit={onSubmit}>
-      <TextField style={{paddingBottom: '10px'}} fullWidth type="number" name="guess" inputProps={{ min: "0", max: "100", step: "1" }} label="Enter your guess..." required />
+      <TextField style={{ paddingBottom: '10px' }} fullWidth type="number" name="guess" inputProps={{ min: "0", max: "100", step: "1" }} label="Enter your guess..." required />
       <Button fullWidth variant="contained" color="primary" type="submit">Guess</Button>
     </form>
   );
--- src/components/Progress/Progress.js
+++ src/components/Progress/Progress.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import {  Divider } from '@material-ui/core';
-
 import './Progress.css';
 
 const Progress = ({ attempt, guessList }) => (
